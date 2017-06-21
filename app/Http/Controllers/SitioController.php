017f55418d4a8d8817336f7c2de02511703220d8
Juan Salgado <juano470@gmail.com>
2017-06-20 20:32:41 -0500

    modificando controlador

--- app/Http/Controllers/SitioController.php
+++ app/Http/Controllers/SitioController.php
@@ -6,15 +6,16 @@ use Illuminate\Http\Request;
 
 class SitioController extends Controller
 {
-<<<<<<< HEAD
-    
-    public function saludar(Request $request, $nombre){
-    	echo "Hello " . $nombre;
-=======
-    public function index(){
 
-echo "Hola mundo";
+	
+	public function saludar(Request $request, $nombre){
+		echo "Hello " . $nombre;
+	}
 
->>>>>>> 1a6eb4078374b342f97170521b36e384d442e9fe
-    }
+	public function index(){
+
+		echo "Hola mundo";
+
+
+	}
 }
